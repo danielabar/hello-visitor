6b50352359aa1d8fea86074b6e2f23c1ef1d87bc
Daniela Baron <baron.daniela@gmail.com>
2021-03-20 13:27:18 -0400

    only allow specific resource for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,6 +2,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:8000'
-    resource '*', headers: :any, methods: %i[post]
+    resource '/visits', headers: :any, methods: %i[post]
   end
 end
