713df534a10512313c61dd5b39576cd7e2669de6
Daniela Baron <baron.daniela@gmail.com>
2021-03-30 15:53:58 -0400

    add visit model tests

--- spec/requests/visits_spec.rb
+++ spec/requests/visits_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Visits", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/visits/index"
+RSpec.describe 'Visits', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/visits/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/visits/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/visits/show'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/visits/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/visits/create'
       expect(response).to have_http_status(:success)
     end
   end
