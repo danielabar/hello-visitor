0364daeb26a943633869fcec45fed39ab8662f07
Daniela Baron <baron.daniela@gmail.com>
2021-04-04 11:06:16 -0400

    tests for referrer

--- spec/factories/visit.rb
+++ spec/factories/visit.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
     user_agent { Faker::Internet.user_agent }
     url { Faker::Internet.url(host: 'example.com', scheme: 'https') }
     remote_ip { Faker::Internet.ip_v4_address }
+
+    trait :google do
+      referrer { 'https://www.google.com' }
+    end
+
+    trait :twitter do
+      referrer { 'https://t.co' }
+    end
   end
 end
