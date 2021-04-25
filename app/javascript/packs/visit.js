17f1bb5d1dde91c6c90eceb10292793237f25969
Daniela Baron <baron.daniela@gmail.com>
2021-04-25 11:04:14 -0400

    make charts stand out a little from the page and update titles

--- app/javascript/packs/visit.js
+++ app/javascript/packs/visit.js
@@ -1,7 +1,8 @@
 // A first attempt at canvas accessibility
 Chartkick.eachChart( (chart) => {
-  console.dir(chart.getOptions())
+  // console.dir(chart.getOptions())
   // console.dir(chart.getData())
+  chart.element.className = 'chart';
   canvasEl = chart.getElement().querySelector('canvas');
   canvasEl.setAttribute('role', 'graphics-document'); // https://www.w3.org/TR/graphics-aria-1.0/
   canvasEl.setAttribute('aria-label', chart.getOptions().library.plugins.title.text);
