a589d18ff1b6c894ed830e9b41bbf8c0608c0cff
Daniela Baron <baron.daniela@gmail.com>
2021-12-26 15:31:35 -0500

    add stats test for filter by url

--- spec/models/stats_spec.rb
+++ spec/models/stats_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Stats, type: :model do
   describe '#collect' do
-    # TODO: Generate more data and test search by url
     let!(:visit1) { FactoryBot.create(:visit, :google, url: 'https://ex.com/p1', created_at: Time.zone.now - 5.days) }
     let!(:visit2) { FactoryBot.create(:visit, :twitter, url: 'https://ex.com/p1', created_at: Time.zone.now - 1.day) }
     let!(:visit3) { FactoryBot.create(:visit, :twitter, url: 'https://ex.com/p2', created_at: Time.zone.now - 1.day) }
@@ -30,6 +29,26 @@ RSpec.describe Stats, type: :model do
       expect(stats.by_referrer[0][1]).to eq(2)
       expect(stats.by_referrer[1][0]).to eq('https://www.google.com')
       expect(stats.by_referrer[1][1]).to eq(1)
+    end
+
+    it 'Filters by given url' do
+      url = 'p2'
+      stats = Stats.new(Time.zone.now - 1.year, Time.zone.now, url)
+      stats.collect
+
+      expect(stats.summary[:min_visits]).to eq(1)
+      expect(stats.summary[:max_visits]).to eq(1)
+      expect(stats.summary[:avg_daily_visits]).to eq(1)
+      expect(stats.summary[:median_daily_visits]).to eq(1)
+
+      expect(stats.by_page[0][0]).to eq('https://ex.com/p2')
+      expect(stats.by_page[0][1]).to eq(1)
+
+      expect(stats.by_date[0][0]).to eq(visit3.created_at.strftime('%Y-%m-%d'))
+      expect(stats.by_date[0][1]).to eq(1)
+
+      expect(stats.by_referrer[0][0]).to eq('https://t.co')
+      expect(stats.by_referrer[0][1]).to eq(1)
     end
   end
 end
