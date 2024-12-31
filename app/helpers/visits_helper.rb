01e44c55850189cfe4f77d5b4a56ad1878f0f6a2
Daniela Baron <baron.daniela@gmail.com>
2024-12-31 07:36:56 -0500

    clear out todo

--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -3,7 +3,6 @@
 module VisitsHelper
   def quick_filter_link(label, range, current_range, path)
     is_active = range[:start_date] == current_range[:start_date] && range[:end_date] == current_range[:end_date]
-    # TODO: Maybe remove rounded-md because it looks weird in the middle, but how would this look if its the end?
     if is_active
       content_tag(:span, label,
                   class: "px-4 py-2 bg-indigo-100 text-indigo-600 font-medium cursor-not-allowed rounded-md")
